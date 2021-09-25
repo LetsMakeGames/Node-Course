fc93843909e0b3c56a3db08bc9e8499190334b1b
William Barnes <jakebarnes1989@gmail.com>
2021-09-25 19:23:32 -0400

    Playground
    - Playground testing for reading and writing to a file

---
+++ playground/1-json.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+data.name = "William"
+data.age = 32
+
+const profile = JSON.stringify(data)
+fs.writeFileSync('1-json.json', profile)
+
+console.log(fs.readFileSync('1-json.json').toString())
