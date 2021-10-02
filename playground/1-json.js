8d2d5e72323b49fd863cf523f8e041e04767b9e7
William Barnes <jakebarnes1989@gmail.com>
2021-10-02 16:16:00 -0400

    Update 1-json.js

--- playground/1-json.js
+++ playground/1-json.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
+
 data.name = "William"
 data.age = 32
 
