b9c8ba1825f68419012d3d98711a2430949cf8be
William Barnes <jakebarnes1989@gmail.com>
2021-10-05 14:47:29 -0400

    Create input.js
    - Implemented input abstraction for getting user input.
    - Add callback functionality so app.js can be kept clean.

---
+++ weather-app/input.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')()
+const chalk = require('chalk')
+
+const getInput = (callback) => {
+
+    let input = prompt('Please enter your location: ')
+
+    while (input === '') {
+        console.log(chalk.red.inverse('A location is required') + '\n')
+        input = prompt('Please enter your location: ')
+    }
+    
+    // Generate URL friendly location for use in API call.
+    const location = encodeURIComponent(input)
+    
+    callback(location)
+}
+
+module.exports = {
+    getInput: getInput
+}
