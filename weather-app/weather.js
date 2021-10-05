b097d23ea103fe099ebf0a31d3b8a088eb8dd008
William Barnes <jakebarnes1989@gmail.com>
2021-10-05 11:44:10 -0400

    Error Handling
    - Implemented error handling for reporting low level errors.
    - Implemented error handling for HTTP response errors.

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -6,14 +6,27 @@ weatherRequest = (longitude, latitude) => {
     const weatherURL = `http://api.weatherstack.com/current?access_key=${weatherToken}&query=${latitude},${longitude}&units=f`
 
     request({ url: weatherURL, json: true }, (error, response) => {
-        const data = response.body
-        const weather = data.current.weather_descriptions[0]
-        const temperature = data.current.temperature
-        const humidity = data.current.humidity
-        const feelsLike = data.current.feelslike
-    
-        console.log('It is currently ' + weather.toLowerCase() + ' with a temperature of ' + temperature + ' degrees.')
-        console.log('It feels like ' + feelsLike + ' degrees with a humidity of ' + humidity + '%.')
+
+        if (error) {
+            console.log('Issue connecting to weatherstack API')
+            console.log('Error Code: ' + error.code)
+            console.log('Error: ' + error.error)
+            
+        } else if (response.body.error) {
+            console.log('Status Code: ' + response.body.error.code)
+            console.log('Error: ' + response.body.error.info)
+        } else {
+            const data = response.body
+            const weather = data.current.weather_descriptions[0]
+            const temperature = data.current.temperature
+            const humidity = data.current.humidity
+            const feelsLike = data.current.feelslike
+        
+            console.log('It is currently ' + weather.toLowerCase() + ' with a temperature of ' + temperature + ' degrees.')
+            console.log('It feels like ' + feelsLike + ' degrees with a humidity of ' + humidity + '%.')
+        }
+
+
     })
 }
 
