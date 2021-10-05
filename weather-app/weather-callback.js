b702c4e10750e30b686d51af37a7efc07e65fc98
William Barnes <jakebarnes1989@gmail.com>
2021-10-05 14:53:18 -0400

    Create weather-callback.js
    - Refactored weather.js to support callbacks and cleaned up abstraction.
    - Implemented weatherData object for sending back a bundle of weatherData for use in app-callback.js

---
+++ weather-app/weather-callback.js
@@ -0,0 +1,38 @@
+const weatherToken = process.env.WEATHER_API_ACCESS_TOKEN
+const request = require('postman-request')
+
+const weatherRequest = (longitude, latitude, callback) => {
+
+    const weatherURL = `http://api.weatherstack.com/current?access_key=${weatherToken}&query=${latitude},${longitude}&units=f`
+
+    request({ url: weatherURL, json: true }, (error, response) => {
+
+        if (error) {
+            console.log('Issue connecting to weatherstack API')
+            console.log('Error Code: ' + error.code)
+            console.log('Error: ' + error.error)
+            
+        } else if (response.body.error) {
+            console.log('Status Code: ' + response.body.error.code)
+            console.log('Error: ' + response.body.error.info)
+        } else {
+            const data = response.body
+            const weather = data.current.weather_descriptions[0]
+            const temperature = data.current.temperature
+            const humidity = data.current.humidity
+            const feelsLike = data.current.feelslike
+            const weatherData = {
+                weather: weather.toLowerCase(),
+                temperature: temperature,
+                feelsLike: feelsLike,
+                humidity: humidity
+            }
+        
+            callback(weatherData)
+        }
+    })
+}
+
+module.exports = {
+    weatherRequest: weatherRequest
+}
