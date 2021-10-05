1afd902d2f581a8ff49fb32f76d2db6e8f56f25a
William Barnes <jakebarnes1989@gmail.com>
2021-10-05 14:49:59 -0400

    Create app-callback.js
    - Abstracted functions to modules to clean up app.js
    - Refactored app.js to support callbacks.

---
+++ weather-app/app-callback.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const input = require('./input')
+const geomap = require('./geomap-callback')
+const weather = require('./weather-callback')
+
+// Get users location input
+input.getInput((location) => {
+
+    // Submit location to geoRequest() to get geodata for weatherRequest.
+    geomap.geoRequest(location, (geodata) => {
+
+        console.log(geodata.place)
+
+        // Submit geodata coordinates to weatherRequest and report the weather
+        weather.weatherRequest(geodata.longitude, geodata.latitude, (weatherData) => {
+
+            // Log a simple weather summary report.
+            console.log('It is currently ' + weatherData.weather + ' with a temperature of ' + weatherData.temperature + ' degrees.')
+            console.log('It feels like ' + weatherData.feelsLike + ' degrees with a humidity of ' + weatherData.humidity + '%.')
+
+        })
+    })    
+})
