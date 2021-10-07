8643fd7f707dc88d898a1aaeed54e4c56e89a83d
William Barnes <jakebarnes1989@gmail.com>
2021-10-07 14:03:49 -0400

    Update app.js
    - Testing out sending back JSON, HTML, and plain text based on a specific route.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>The Weather App</h1>')
 })
 
 app.get('/help', (req, res) => {
@@ -11,11 +11,14 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.send('This is a simple express server for serving up web apps!')
+    res.send('<h1>The Weather App</h1> <p>This is a simple express server for serving up a weather app!</p>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather info to be added in a future update.')
+    res.send({
+        location: 'East Lansing, MI',
+        forecast: 'It is currently mist with a temperature of 66 degrees. It feels like undefined degrees with a humidity of 90%.'
+    })
 })
 
 app.listen(3000, () => {
